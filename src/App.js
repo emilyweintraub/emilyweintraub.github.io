5ce807c52c3bf042a321a7ef1b25b7ae520075a1
emilyweintraub <emilyweintraub2021@u.northwestern.edu>
2020-09-21 14:37:19 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import Nav from './components/navbar'
+import Home from './components/home.js'
+import About from './components/about.js'
+import Projects from './components/projects.js'
+import Contact from './components/contact.js'
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Router>
+          <Nav />
+          <Switch>
+            <Route path="/home" exact component={Home} />
+            <Route path="/about" exact component={About} />
+            <Route path="/projects" exact component={Projects} />
+            <Route path="/contact" exact component={Contact} />
+          </Switch>
+        </Router>
       </header>
     </div>
   );
---
+++ src/components/navbar.js
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+import '../styles/Nav.css';
+import logo from '../images/esw.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
+import { Link } from "react-router-dom";
+
+// class NavBar extends Component {
+const NavBar = () => {
+  function sayHello() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      // alert('Hello!');
+      x.className += " responsive";
+    }
+    else {
+      x.className = "topnav";
+    }
+  }
+  // render() {
+  return (
+    <React.Fragment>
+      <nav className="navbar navbar-dark bg-dark mb-3 sticky-nav">
+        <a className="App-logo"><Link to={{ pathname: `/home.js` }}>esw.</Link></a>
+        <a href="javascript:void(0);" class="ico" onClick={sayHello}>&#9776;</a>
+        <a className="topnav" id="myTopnav">
+          <div className="dropdown">
+          <Link class="drop" to={{ pathname: `/projects.js` }}><a>+ Projects</a></Link>
+          <div class="dropdown-content">
+              <a href="#">Link 1</a>
+              <a href="#">Link 2</a>
+              <a href="#">Link 3</a>
+            </div>
+          </div>  
+          <Link to={{ pathname: `/about.js` }}><a>About Me</a></Link>
+          <Link to={{ pathname: `/contact.js` }}><a>Contact</a></Link>
+        </a>
+        <a className="icons" id="myTopnav">
+          <Link to={{ pathname: `/contact.js` }}><FontAwesomeIcon icon={faEnvelope} className="icon" /></Link>
+          <a href='http://www.linkedin.com/in/emilyweintraub'><FontAwesomeIcon icon={faLinkedinIn} className="icon" /></a>
+        </a>
+
+        {/* <a>Hey Emily!</a>
+          {/* <a className="navbar-brand" href="#">
+            <h1>Total Items <span className="badge badge-secondary">{this.props.totalItems}</span></h1>
+          </a> */}
+      </nav>
+    </React.Fragment>
+  );
+  // }
+}
+
+export default NavBar;
+
+// class NavBar extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <nav className="navbar navbar-dark bg-dark mb-3 sticky-nav">
+//         <a className="App-logo">esw.</a>
+//         <a className="all">
+//           <a>+ Projects</a>
+//           <a>About Me</a>
+//           <a>Contact</a>
+//         </a>
+//           <a className="icons">
+//           <FontAwesomeIcon icon={faEnvelope} className="icon" />
+//           <FontAwesomeIcon icon={faLinkedinIn} className="icon" />
+//           </a>
+
+//           {/* <a>Hey Emily!</a>
+//           {/* <a className="navbar-brand" href="#">
+//             <h1>Total Items <span className="badge badge-secondary">{this.props.totalItems}</span></h1>
+//           </a> */}
+//         </nav>
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+// export default NavBar;
