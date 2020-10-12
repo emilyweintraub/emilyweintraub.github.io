50a562fed0effa59031c92a725ce83bfab55c511
emilyweintraub <emilyweintraub2021@u.northwestern.edu>
2020-10-12 01:59:17 -0500

    -edited experience section

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import '../styles/Home.css'
 import resume from '../images/Resume.pdf'
+import nodi from '../images/nodi.png'
+import elcare from '../images/elcare.png'
+import peerpal from '../images/peerpal.png'
 
 class Home extends Component {
     targetRef = React.createRef()
@@ -23,44 +26,45 @@ class Home extends Component {
                     </p>
                 </div>
                 <div className='Projects' ref={ref => { this.targetRef = ref }}>
-                    <h1 className='Projects-Header'>Projects</h1>
+                    <h1 className='Projects-Header'>Experience</h1>
                     <div className='Projects-Details'>
-                        <div className='Project L'>
+                        <div className='Project R'>
+                        <div>
+                                <h2 className="Title">PeerPal</h2>
+                                <ul className="Project-Description">
+                                    <li>Individually responsible for design and implementation of front end of web application using next.js server side rendering with an express server</li>
+                                    <li>Dynamically updated postgresql database based on user action and algorithm results</li>
+                                    <li>Assisted in full stack-development of broader tool as well as high level technical direction of the development of custom API</li>
+                                    </ul>
+                            </div>
                             <div className="Project-Picture">
-                                <img alt="" />
+                                <img alt="" src={peerpal}/>
+                            </div>
+                        </div>
+                        <div className='Project L'>
+                        <div className="Project-Picture">
+                                <img alt="" src={nodi} />
                             </div>
                             <div>
-                                <h2 className="Title">PeerPal</h2>
-                                <p className="Project-Description">
-                                    • Individually responsible for design and implementation of front end of web application using next.js server side rendering with an express server
-                                    • Dynamically updated postgresql database based on user action and algorithm results
-                                    • Assisted in full stack-development of broader tool as well as high level technical direction of the development of custom API</p>
+                                <h2 className="Title">Northwestern Open Data Portal</h2>
+                                <ul className="Project-Description">
+                                <li>Developed site’s navigational user interface using React</li>
+                                <li>Aggregated public student data from University data books, directors of research, and student surveys</li>
+                                <li>Pitched and developed relationship with Northwestern’s Office of Institutional Research to obtain data resources and university support</li>
+                                </ul>
                             </div>
                         </div>
                         <div className='Project R'>
-                            <div>
-                                <h2 className="Title">NODI: Northwestern Open Data Portal</h2>
-                                <p className="Project-Description">
-                                    • Developed site’s navigational user interface using React
-                                    • Aggregated public student data from University data books, directors of research, and student surveys
-                                    • Pitched and developed relationship with Northwestern’s Office of Institutional Research to obtain data resources and university support
-                                </p>
-                            </div>
-                            <div className="Project-Picture">
-                                <img alt="" />
-                            </div>
-                        </div>
-                        <div className='Project L'>
-                            <div className="Project-Picture">
-                                <img alt="" />
-                            </div>
-                            <div>
+                        <div>
                                 <h2 className="Title">ElCare</h2>
-                                <p className="Project-Description">
-                                    • Ideated, developed and pitched ElCare, an application that allows family members to quickly check in with elderly loved ones online
-                                    • Conducted user testing with Northwestern students to prototype and adjust ElCare UX
-                                    • Created and designed application’s UI using Figma and React
-                                </p>
+                                <ul className="Project-Description">
+                                <li>Ideated, developed and pitched ElCare, an application that allows family members to quickly check in with elderly loved ones online</li>
+                                    <li>Conducted user testing with Northwestern students to prototype and adjust ElCare UX</li>
+                                    <li>Created and designed application’s UI using Figma and React</li>
+                                </ul>
+                            </div>
+                            <div className="Project-Picture">
+                                <img alt="" src={elcare}/>
                             </div>
                         </div>
                     </div>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -25,7 +25,7 @@ const NavBar = () => {
         <a className="topnav" id="myTopnav">
           <Link to={{ pathname: `/`, state: { fromBanner: true } }} onClick={location.pathname === 'home.js' ? this.targetRef.scrollIntoView({
             behavior: 'smooth'
-          }) : undefined}><a>Projects</a></Link>
+          }) : undefined}><a>Experience</a></Link>
           <Link to={{ pathname: `/about` }} onClick={scrollTop}><a>About Me</a></Link>
           <Link to={{ pathname: `/about`, state: { fromBanner: true } }} onClick={location.pathname === 'about.js' ? this.targetRef.scrollIntoView({
             behavior: 'smooth'
